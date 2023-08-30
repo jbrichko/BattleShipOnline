f77afe643d1996c7e5c6d00286d7d42330bbfbe9
jbrichko <jakub.brichkovskiy@gmail.com>
2023-08-30 18:52:11 +0200

    Dokumentacja doxygen dla klasy board.hpp

--- battlesziponlajn/Board.hpp
+++ battlesziponlajn/Board.hpp
@@ -4,13 +4,30 @@
 
 #include "Ship.hpp"
 
+/*!	\class Board
+*	\brief Jest to klasa, która jest odpowiedzialna za planszê gry.
+*/
 class Board
 {
 public:
 
-	static const int size = { 10 };						//d³ugoœæ mapy - sta³a wartoœæ równa 10
+	/**
+	*	Klasa ta posiada najwa¿niesze informacje o tym co znajduje siê w danym polu planszy oraz
+	*	drukuje planszê w terminalu.
+	*/
 
-	enum FieldStatus : char							//status jednego pola mapy
+
+	/**
+	*	D³ugoœæ mapy - sta³a wartoœc równa 10.
+	*/
+	static const int size = { 10 };
+
+	/**
+	*	\enum FieldStatus
+	*	Enumerator opisuj¹cy wszystkie mo¿liwe opcje jakie mo¿e przybraæ pole planszy. W danym momencie pole mo¿e 
+	*	mieæ przypisan¹ tylko jedn¹ opcjê.
+	*/
+	enum FieldStatus : char
 	{
 		empty = '~',
 		miss = '*',
@@ -19,37 +36,104 @@ public:
 		ship = 'S', 
 	}; 
 
+	/**
+	*	Status pola planszy.
+	*/
 	FieldStatus** board;
 
-	void print(const char* title);					//metoda macierzysta, która drukuje plansze w terminalu
+	/**
+	*	\brief Drukuje planszê.
+	*	\param title Tytu³ danej planszy, "YOUR SHIPS" lub "YOUR SHOTS".
+	*	Metoda macierzysta, która drukuje planszê w terminalu. Jest rozbudowywana w klasach pochodnych.
+	*/
+	void print(const char* title);
 
+	/**
+	*	Domyœlny konstruktor obiektów tej klasy.
+	*/
 	Board();
+
+	/**
+	*	Domyœlny dekonstruktor obiektów tej klasy.
+	*/
 	~Board();
 };
 
-class PlayerBoard : public Board					//klasa obs³uguj¹ca plansze ze statkami gracza 
+
+/*!	\class PlayerBoard
+*	\brief Jest to pochodna klasy Board. Odpowiada g³ównie za prawid³owe umieszczenie statków na planszy.
+*/
+class PlayerBoard : public Board
 {
 public: 
 
-	static const int NO_OF_BATTLESHIPS = { 2 };		//iloœæ statków danego typu
-	static const int NO_OF_CRUISERS = { 3 };		//
-	static const int NO_OF_DESTROYERS = { 4 };		//
+	/**
+	*	Klasa ta umieszcza odpowiada za odpowiednie u³o¿enie statków na planszy, tj. nie mog¹ siê na siebie
+	*	nak³adaæ lub dotykaæ. Nastêpnie wedle tego warunku ustawia statki na planszy. W klasie tej jest równie¿
+	*	metoda, która zlicza niezatopione statki gracza.
+	*/
 
+	/**
+	*	Sta³e opisuj¹ce iloœæ statków danego typu.
+	*/
+	static const int NO_OF_BATTLESHIPS = { 2 };
+	static const int NO_OF_CRUISERS = { 3 };
+	static const int NO_OF_DESTROYERS = { 4 };
+
+	/**
+	*	WskaŸnik na wskaŸnik do obiektu klasy Ship.
+	*/
 	Ship** ships;
+
+	/**
+	*	Sumaryczna iloœæ statków jednego gracza.
+	*/
 	int noOfShips;
 
-	void print();									//drukowanie planszy ze statkami gracza
-	int getShipsCount();							//liczy iloœæ statków niezatopionych gracza
-	void placeShips();								//umieszcza staki na planszy
-	bool isPlacementGood(int currentShipID);		//sprawdza czy wylosowane wspó³rzêdne s¹ odpowiednie, by statek siê nie dotyka³ z innymi ju¿ umieszczonymi na planszy
+	/**
+	*	Drukuje w terminalu planszê ze statkami gracza.
+	*/
+	void print();
 
-	PlayerBoard(); 
+	/**
+	*	\brief Liczy iloœæ niezatopionych statków gracza na planszy.
+	*	\return Zwraca liczbê statków niezatopionych.
+	*/
+	int getShipsCount();
+
+	/**
+	*	Umieszcza statki na planszy pseudolosowym po³o¿eniu za pomoc¹ pêtli. Warunkiem dobrego po³o¿enia jest 
+	*	metoda "isPlacementGood".
+	*/
+	void placeShips();
+
+	/**
+	*	\brief Sprawdza wylosowane wspó³rzêdne i orientacje statku pod k¹tem poprawnoœci umieszczenia na planszy.
+	*	\param currentShipID Indywidulany numer danego statku.
+	*	\return Zwraca "false" w przypadku b³êdnego umieszczenia.
+	*/
+	bool isPlacementGood(int currentShipID);
+	
+	/**
+	*	Domyœlny konstruktor obiektów tej klasy.
+	*/
+	PlayerBoard();
+
+	/**
+	*	Domyœlny dekonstruktor obiektów tej klasy.
+	*/
 	~PlayerBoard();
 };
 
-class EnemyBoard : public Board						//klasa obs³uguj¹ca plansze ze statkami wroga (twoje strza³y)
+/*!	\class EnemyBoard
+*	\brief Jest to pochodna klasy Board. Odpowiada g³ównie za obs³ugê planszy ze statkami wroga - strza³ami gracza.
+*/
+class EnemyBoard : public Board	
 {
 public: 
 
-	void print();									//drukowanie planszy ze statkami wroga (twoimi strza³ami) 
+	/**
+	*	Drukuje w terminalu planszê ze statkami wroga - strza³ami gracza.
+	*/
+	void print(); 
 };
