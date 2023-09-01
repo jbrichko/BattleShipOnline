265a37e39481d61f16e3d61e09dc5d42d1376ca4
jbrichko <jakub.brichkovskiy@gmail.com>
2023-09-01 17:31:01 +0200

    Komcie w pliku ship.cpp

--- battlesziponlajn/Ship.cpp
+++ battlesziponlajn/Ship.cpp
@@ -9,30 +9,37 @@
 #endif // DEBUG
 
 
-
+///	Funkcja sprawdzaj¹ca czy statek jest zatopiony
 bool Ship::checkIfSunk(void)
 {
+	///	Iteracja po wszystkich polach statku, która sprawdza czy któraœ z nich jest oznaczona liter¹ "S", co oznacza,
+	///	¿e statek jest zatopiony.
 	for (int i = 0; i < size; i++)
 	{
-		if (deck[i] == 'S') return true;
+		///	Zwraca true jeœli jest zatopiony.
+		if (deck[i] == 'X') return true;
 	}
-
+	/// Zwraca false jeœli nie jest zatopiony.
 	return false;
 }
 
+///	Funkcja losuj¹ca pseudolosowe wspó³rzêdne pocz¹tku statku.
 void Ship::randomPlace(int boardSize)
 {
-
+	///	Zmienna, która okreœla czy funkcja srand() zosta³a ju¿ wykonana. Pocz¹tkowo ustalona na wartoœæ false.
 	static bool didSrandExecute = false; 
 
 	if(didSrandExecute == false)
 	{
+		///	Wywo³anie funkcji srand(), po jej wykonaniu zmienia wartoœæ znacznika didSrandExecute na true. Oznacza to, ¿e funkcja ta zosta³a wywo³ana.
 		srand(time(nullptr)); 
 		didSrandExecute = true; 
 	}
 
+	///	Losowanie orientacji statku.
 	orientation = static_cast<Orientation>(rand() % 2); 
 
+	///	Losowanie wspó³rzêdnych w zale¿noœci od orientacji statku.
 	if (orientation == horizontal)
 	{
 		locationX = rand() % (boardSize - size + 1);
@@ -52,18 +59,22 @@ void Ship::randomPlace(int boardSize)
 
 }
 
+///	Domyœlny konstruktor klasy Ship. Tworzony jest w nim statek o polu "S".
 Ship::Ship(ShipSize length)
 	: size(length)
 {
 	deck = new	char[size];
 
+	///	Iteruje po d³ugoœci statku.
 	for (int i = 0; i < size; i++)
 		deck[i] = 'S'; 
 
+	///	Przypisuje wartoœci, które s¹ nieprawid³owe w kontekœcie planszy gry.
 	locationX = -1;
 	locationY = -1;
 }
 
+///	Domyœlny dekonstruktor klasy Ship.
 Ship::~Ship()
 {
 	delete[] deck;
