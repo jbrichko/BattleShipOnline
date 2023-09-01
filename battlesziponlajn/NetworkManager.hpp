6927361ad8f27f7f01977d9867f7a039a88238d0
jbrichko <jakub.brichkovskiy@gmail.com>
2023-09-01 20:28:49 +0200

    komentarze w networkingu

--- battlesziponlajn/NetworkManager.hpp
+++ battlesziponlajn/NetworkManager.hpp
@@ -10,10 +10,20 @@
 #include <asio/ts/internet.hpp>
 
 
-
+/*! \class Network
+*   \brief Jest to klasa macierzysta odpowiadaj¹ca za obs³ugê modu³u sieciowego gry.
+*/
 class Network
 {
 public:
+
+    /**
+    *   Klasa posiada metody, struktury i enumerator s³u¿¹cê do prawid³owego przebiegu gry przez sieæ.
+    *   
+    *   Modu³ sieciowy gry statki wykorzystuje bibliotekê ASIO do multiplatformowej obs³ugi operacji sieciowych.
+    *   ASIO (Asynchronous Input/Output) to potê¿na biblioteka C++, która umo¿liwia asynchroniczn¹ obs³ugê
+    *   operacji wejœcia/wyjœcia, co jest kluczowe dla wydajnej komunikacji sieciowej w grze.
+    */
 
     const static uint32_t DEFAULT_HOST_PORT = { 62137 };
 
@@ -22,42 +32,87 @@ public:
     asio::ip::tcp::endpoint endpoint;
     asio::ip::tcp::socket socket;
 
+
+    /**
+    *	\enum MessageType
+    *	Enumerator opisuj¹cy dane jakie mog¹ zostaæ wys³ane pomiêdzy graczami.
+    *   Typem danych jest unsigned integer 16-bit. 
+    */
     enum MessageType : uint16_t
     {
-        string, //for testing
+        /// string - dla testów i komunikacji miêdzy graczami (to do)
+        string,
+        /// game_start - informacja o rozpoczêciu gry
         game_start,
+        /// shot - informacja o strzale gracza
         shot,
+        /// shot_response - informacja o efekcie strza³u (pud³o lub trafienie)
         shot_response,
     };
 
+    /*!
+    *   \struct MessageHeader
+    *   \brief Header wysy³anej lub odbieranej informacji.
+    */
     struct MessageHeader
     {
+        /// Typ informacji.
         MessageType type;
+        /// Rozmiar informacji.
         uint16_t payloadSize;
     };
 
+    /*!
+    *   \struct ShotMessage
+    *   \brief Struktura posiadaj¹ca wszelkie dane by wys³aæ lub odczytaæ informacje o strzale.
+    */
     struct ShotMessage
     {
+        /// Header strza³u.
         MessageHeader header = { shot, sizeof(ShotMessage) };
+        /// Wspó³rzêdna X strza³u.
         uint8_t x;
+        /// Wspó³rzêdna Y strza³u.
         uint8_t y;
     };
 
     Network() : socket(context) {}
 
+    /**
+    *	\brief Sprawdza status po³¹czenia.
+    *	\return Zwraca informacjê czy zosta³o zawarte po³¹czenie.
+    */
     bool isConnected();
-    bool send(MessageHeader& header, std::vector<uint8_t>& message); 
+    /**
+    *	\brief Wysy³a informacje do innego gracza.
+    *	\return Zwraca inforamcjê czy informacja zosta³a wys³ana.
+    */
+    bool send(MessageHeader& header, std::vector<uint8_t>& message);
+    /**
+    *	\brief Odbiera informacje od innego gracza.
+    *	\return Zwraca inforamcjê czy informacja zosta³a odebrana.
+     */
     bool recive(MessageHeader& header, std::vector<uint8_t>& message);
+    /**
+     *	\brief Zamyka po³¹czenie pomiêdzy graczami.
+     */
     void disconnect(); 
 
 };
 
+/*! \class NetworkHost
+*   \brief Jest to klasa pochodna od klasy Network. Ob³uguje hosta po³¹czenia.
+*/
 class NetworkHost : public Network
 {
 public:
 
     asio::ip::tcp::acceptor acceptor;
 
+    /**
+    *	\brief Odbiera informacje od innego gracza.
+    *	\return Zwraca inforamcjê czy informacja zosta³a odebrana.
+     */
     std::string getLocalIP();
 
     bool waitForConnection(uint16_t port = DEFAULT_HOST_PORT);
